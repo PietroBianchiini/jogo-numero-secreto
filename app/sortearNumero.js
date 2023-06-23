ddfd9ad54e84d0304893dfd41cb3868d21b5a559
Pietro Bianchini <pietrobianchini1@gmail.com>
2023-06-23 18:51:29 -0300

    Upload dos arquivos

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumeroAleatorio();
+const elementoMenorValor = document.getElementById("menor-valor");
+const elementoMaiorValor = document.getElementById("maior-valor");
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log(numeroSecreto)
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
+
